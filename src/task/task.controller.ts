c9128c2eac46f41db90a96df20e074fe8da57057
Evandro Calado <evandrocalado07@gmail.com>
2025-05-12 23:07:08 -0300

    feat: implement CRUD operations in Task module with updated HTTP request examples

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,14 +1,40 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('task')
 export class TaskController {
+  @Post()
+  create(@Body() body: unknown) {
+    return body;
+  }
+
   @Get()
-  findAll() {
+  readAll() {
     return 'This action returns all task';
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  readOne(@Param('id') id: string) {
     return `This action returns a #${id} task`;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: Record<string, any>) {
+    return {
+      id,
+      ...body,
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return `This action removes a #${id} task`;
   }
 }
