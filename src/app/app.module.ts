f563f8ee8cd2904a7a748bfb5b4b001827f67f3a
Evandro Calado <evandrocalado07@gmail.com>
2025-05-16 08:39:59 -0300

    Feat: Configure database connection using ConfigModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,30 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigType } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { TasksModule } from '@/tasks/tasks.module';
 import { UsersModule } from '@/users/users.module';
 
+import appConfig from './app.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: process.env.DATABASE_TYPE as 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: Number(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_DATABASE,
-      autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOAD_ENTITIES),
-      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forFeature(appConfig)],
+      inject: [appConfig.KEY],
+      useFactory: ({ database }: ConfigType<typeof appConfig>) => ({
+        type: database.type,
+        host: database.host,
+        port: database.port,
+        username: database.username,
+        password: database.password,
+        database: database.database,
+        autoLoadEntities: database.autoLoadEntities,
+        synchronize: database.synchronize,
+      }),
     }),
     TasksModule,
     UsersModule,
